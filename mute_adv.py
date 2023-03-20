69e5a4717326ef4c569759bdaf4bbee43f52b1ce
wisdomsoar <42779504+wisdomsoar@users.noreply.github.com>
2023-03-20 14:33:30 +0800

    Update mute_adv.py

--- mute_adv.py
+++ mute_adv.py
@@ -3,17 +3,18 @@ import time
 #regular expression
 import re
 
+pyautogui.FAILSAFE = False
+
 #python的縮排必須正確，要寫在while底下的敘述，就應該要縮排。
 while True:
-    window_title = "Advertisement"
     regex = re.compile('.*Advertisement.*', re.IGNORECASE)
     windows = [window for window in pyautogui.getAllWindows() if regex.match(window.title)]
 
     if len(windows) > 0:
         pyautogui.press('volumemute')
-        print(f"Muted audio for window: {window_title}")
+        print(f"Muted audio!")
         time.sleep(30)
         
         pyautogui.press('volumemute')
-        print(f"Unmuted audio for window: {window_title}")
+        print(f"Unmuted audio!")
     time.sleep(1)
